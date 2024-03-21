7fbbe37d8db42a0cb16de961a32badfdfe8a0538
arturyahnychuk <arturyahnychuk@gmail.com>
2024-03-21 18:52:33 -0300

    preparing the project and making the header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import Header from '@/components/Header.vue';
 
 </script>
 
 <template>
   <div>
-    <h1>Hello World</h1>
+    <Header />
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <h1>Search the Github.</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="https://github.com/arturyahnychuk">
+                        <i class="devicon-github-original"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/artur-yahnychuk">
+                        <i class="devicon-linkedin-plain"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@use "../app.scss";
+header {
+    background: app.$header-bg;
+    padding: 0px 32px;
+    height: 65px;
+    @include app.flex(row, space-between);
+
+    h1 {
+        font-size: 2.5rem;
+        font-family: app.$title-font;
+        color: app.$title;
+    }
+
+    nav {
+        ul {
+            @include app.flex(row, space-between);
+            width: 120px;
+            li {
+                a {
+                    text-decoration: none;
+                    color: white;
+                    i {
+                        font-size: 2.5rem;
+                        transition: .3s;
+
+                        &:hover {
+                            font-size: 2.7rem;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+</style>
